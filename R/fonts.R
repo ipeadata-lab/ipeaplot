158247ba5d82f9b146c20e13ad6193f042c61ce3
PedroJorge7 <pedrojorge_holanda@hotmail.com>
2024-07-11 01:42:03 -0300

    update

--- R/fonts.R
+++ R/fonts.R
@@ -6,28 +6,20 @@
 .onLoad <- function(libname, pkgname) {
 
   path <- system.file("extdata/", package = "ipeaplot")
-
-  suppressMessages({sysfonts::font_add("Roboto-Black", regular = paste0(path,"/ttf/Roboto-Black.ttf"))})
-  suppressMessages({sysfonts::font_add("Roboto-BlackItalic", regular = paste0(path,"/ttf/Roboto-BlackItalic.ttf"))})
-  suppressMessages({sysfonts::font_add("Roboto-Bold", regular = paste0(path,"/ttf/Roboto-Bold.ttf"))})
-  suppressMessages({sysfonts::font_add("Roboto-BoldItalic", regular = paste0(path,"/ttf/Roboto-BoldItalic.ttf"))})
-  suppressMessages({sysfonts::font_add("Roboto-CondensedBold", regular = paste0(path,"/ttf/Roboto-CondensedBold.ttf"))})
-  suppressMessages({sysfonts::font_add("Roboto-CondensedBoldItalic", regular = paste0(path,"/ttf/Roboto-CondensedBoldItalic.ttf"))})
-  suppressMessages({sysfonts::font_add("Roboto-CondensedItalic", regular = paste0(path,"/ttf/Roboto-CondensedItalic.ttf"))})
-  suppressMessages({sysfonts::font_add("Roboto-CondensedLight", regular = paste0(path,"/ttf/Roboto-CondensedLight.ttf"))})
-  suppressMessages({sysfonts::font_add("Roboto-CondensedLightItalic", regular = paste0(path,"/ttf/Roboto-CondensedLightItalic.ttf"))})
-  suppressMessages({sysfonts::font_add("Roboto-CondensedRegular", regular = paste0(path,"/ttf/Roboto-CondensedRegular.ttf"))})
-  suppressMessages({sysfonts::font_add("Roboto-Italic", regular = paste0(path,"/ttf/Roboto-Italic.ttf"))})
-  suppressMessages({sysfonts::font_add("Roboto-Light", regular = paste0(path,"/ttf/Roboto-Light.ttf"))})
-  suppressMessages({sysfonts::font_add("Roboto-LightItalic", regular = paste0(path,"/ttf/Roboto-LightItalic.ttf"))})
-  suppressMessages({sysfonts::font_add("Roboto-Medium", regular = paste0(path,"/ttf/Roboto-Medium.ttf"))})
-  suppressMessages({sysfonts::font_add("Roboto-MediumItalic", regular = paste0(path,"/ttf/Roboto-MediumItalic.ttf"))})
-  suppressMessages({sysfonts::font_add("Roboto-Regular", regular = paste0(path,"/ttf/Roboto-Regular.ttf"))})
-  suppressMessages({sysfonts::font_add("Roboto-Thin", regular = paste0(path,"/ttf/Roboto-Thin.ttf"))})
-  suppressMessages({sysfonts::font_add("Roboto-ThinItalic", regular = paste0(path,"/ttf/Roboto-ThinItalic.ttf"))})
-  suppressMessages({showtext::showtext_auto()})
   suppressMessages({extrafont::font_import(paths = paste0(path,'/ttf/'), prompt = F)})
-  extrafont::loadfonts(quiet = TRUE)
+  #suppressMessages({extrafont::font_import(pattern="Roboto-",prompt = FALSE)})
+  suppressMessages({extrafont::loadfonts(quiet = TRUE)})
 }
 
+.onAttach <- function(libname, pkgname) {
+  roboto_fonts <- c("RobotoCondensed-Light")
 
+  sys_fonts <- systemfonts::system_fonts()
+  missing_fonts <- roboto_fonts[!(roboto_fonts %in% sys_fonts$name)]
+
+  if (length(missing_fonts) > 0) {
+    packageStartupMessage("The following Roboto fonts are missing, please install them: ", paste(missing_fonts, collapse = ", "))
+  } else {
+    packageStartupMessage("All Roboto fonts are installed.")
+  }
+}
